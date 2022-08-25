a68b9c6f873f63d15efa7bea1e9fa640227cf650
LucasPCotrim <lucaspcotrim@gmail.com>
2022-08-24 23:19:54 -0300

    Implemented GET request on /tweets route

--- src/app.js
+++ src/app.js
@@ -21,6 +21,20 @@ app.post('/tweets', (req, res) => {
   res.send('OK');
 });
 
+// GET /tweets
+app.get('/tweets', (req, res) => {
+  const lastTweets = tweets.slice(-10);
+
+  const lastTweetsWithAvatar = lastTweets.map((tweet) => {
+    const avatar = users.find((user) => {
+      return user.username === tweet.username;
+    }).avatar;
+    return { ...tweet, avatar: avatar };
+  });
+
+  res.send(lastTweetsWithAvatar);
+});
+
 // Initialize server
 app.listen(5000, () => {
   console.log('Server listening on port 5000');
